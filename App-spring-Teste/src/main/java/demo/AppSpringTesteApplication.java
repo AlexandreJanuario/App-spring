27799f280d6219c0eaf2b379b372055fa25266e8
CristianoAzevedo <cristianodasilva.azevedo>
2015-02-21 15:48:44 -0200

    Configuração de projetosssssss

---
+++ App-spring-Teste/src/main/java/demo/AppSpringTesteApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringTesteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSpringTesteApplication.class, args);
+    }
+}
