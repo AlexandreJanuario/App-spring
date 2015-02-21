db9299dc3cf2e1c0b108012c8a92e5417ca7963d
CristianoAzevedo <cristianodasilva.azevedo@gmail.com>
2015-02-21 16:25:56 -0200

    configuração do projeto

--- src/main/java/demo/ServletInitializer.java
+++ src/main/java/demo/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AppSpringTesteApplication.class);
+		return application.sources(DemoApplication.class);
 	}
 
 }
